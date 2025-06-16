60355dbd75359439b11866bd70ee951ff2f84031
rhea-boop <telistempelis@gmail.com>
2025-06-16 21:01:05 +0300

    I've heard enough of this for a lifetime.

--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 ANSIBLE_HOST_KEY_CHECKING=False \
 ansibleplaybook ${YAML} -i ${inventoryfile}   -f 5  -u ${User}  \
---private-key=/keys/fedlab \
+--private-key="keys/fedlab" \
 -e ansible_python_interpreter=/usr/bin/python3 \
 --extra-vars "username=${User} inv=${inventory}" \
 --ssh-common-args='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
